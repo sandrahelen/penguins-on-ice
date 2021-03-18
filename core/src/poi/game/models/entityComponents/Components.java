7f4db97872267ec7ffdc9acdd4f756b66e5e930b
Sandra Helen Husnes <sandrahh@stud.ntnu.no>
2021-03-18 12:46:02 +0100

    update components/movementsystem

--- core/src/poi/game/models/entityComponents/Components.java
+++ core/src/poi/game/models/entityComponents/Components.java
@@ -1,5 +1,5 @@
 package poi.game.models.entityComponents;
-import poi.game.models.entityComponents.Animation;
+import poi.game.models.entityComponents.SpriteAnimation;
 import poi.game.models.entityComponents.Boost;
 import poi.game.models.entityComponents.Collidable;
 import poi.game.models.entityComponents.Position;
