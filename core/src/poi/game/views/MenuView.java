67d28e1217ab08a8d6c397ea506cac3e05a2ac6b
Marit <maritfha@stud.ntnu.no>
2021-03-24 08:56:50 +0100

    added buttons from figma to menuView

--- core/src/poi/game/views/MenuView.java
+++ core/src/poi/game/views/MenuView.java
@@ -10,6 +10,7 @@ import poi.game.models.factories.ViewFactory;
 
 public class MenuView extends View {
 
+    private Texture titlePoI;
     private Texture buttonPlay;
     private Texture buttonHighscore;
     private Texture buttonSettings;
@@ -21,9 +22,10 @@ public class MenuView extends View {
     public MenuView(MenuController controller) {
         super(controller);
         cam.setToOrtho(false, Poi.WIDTH, Poi.HEIGHT);
-        buttonPlay = new Texture("button.png");
-        buttonHighscore = new Texture("button.png");
-        buttonSettings = new Texture("button.png");
+        titlePoI = new Texture("titlePoI.png");
+        buttonPlay = new Texture("buttonPlay.png");
+        buttonHighscore = new Texture("buttonHighscore.png");
+        buttonSettings = new Texture("buttonSettings.png");
         text = new BitmapFont();
         boundsPlay = new Rectangle(Poi.WIDTH/4, (Poi.HEIGHT - buttonPlay.getHeight())*3/6 - buttonPlay.getHeight()/2, buttonPlay.getWidth(), buttonPlay.getHeight());
         boundsHighscore = new Rectangle(Poi.WIDTH/4, (Poi.HEIGHT - buttonPlay.getHeight()/2)*4/6 - buttonHighscore.getHeight()/2, buttonHighscore.getWidth(), buttonHighscore.getHeight());
@@ -43,7 +45,7 @@ public class MenuView extends View {
                 controller.set(new SettingsView(controller));
             }
         }
-        /*
+
         if (boundsPlay.contains(Gdx.input.getX(), Gdx.input.getY())) {
             Gdx.app.log("GAME", "[" + Gdx.input.getX() + ", " + Gdx.input.getY() +"]");
         }
@@ -53,7 +55,7 @@ public class MenuView extends View {
         else if (boundsSettings.contains(Gdx.input.getX(), Gdx.input.getY())) {
             Gdx.app.log("SETTINGS", "[" + Gdx.input.getX() + ", " + Gdx.input.getY() +"]");
         }
-         */
+
 
     }
 
@@ -67,15 +69,16 @@ public class MenuView extends View {
         //Gdx.app.log("MenuView", "render");
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        text.setColor(94/255f,169/255f,186/255f,1);
-        text.draw(sb, "Penguins on Ice", 40,Poi.HEIGHT - 50);
-        text.setColor(1, 1, 1, 1);
+        //text.setColor(94/255f,169/255f,186/255f,1);
+        //text.draw(sb, "Penguins on Ice", 40,Poi.HEIGHT - 50);
+        sb.draw(titlePoI, Poi.WIDTH/8, Poi.HEIGHT - buttonPlay.getHeight()*3/2);
+        //text.setColor(1, 1, 1, 1);
         sb.draw(buttonPlay, Poi.WIDTH/4, Poi.HEIGHT*3/6);
-        text.draw(sb, "Play game", Poi.WIDTH*2/5, Poi.HEIGHT*3/6 + buttonPlay.getHeight()*3/5);
+        //text.draw(sb, "Play game", Poi.WIDTH*2/5, Poi.HEIGHT*3/6 + buttonPlay.getHeight()*3/5);
         sb.draw(buttonHighscore, Poi.WIDTH/4,Poi.HEIGHT*2/6);
-        text.draw(sb, "Highscore", Poi.WIDTH*2/5, Poi.HEIGHT*2/6 + buttonHighscore.getHeight()*3/5);
+        //text.draw(sb, "Highscore", Poi.WIDTH*2/5, Poi.HEIGHT*2/6 + buttonHighscore.getHeight()*3/5);
         sb.draw(buttonSettings, Poi.WIDTH/4,Poi.HEIGHT/6);
-        text.draw(sb, "Settings", Poi.WIDTH*2/5, Poi.HEIGHT/6 + buttonSettings.getHeight()*3/5);
+        //text.draw(sb, "Settings", Poi.WIDTH*2/5, Poi.HEIGHT/6 + buttonSettings.getHeight()*3/5);
         sb.end();
 
     }
