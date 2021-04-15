eed056f7f859e9394d3ba3ded0d541782a117e5c
Sandra Helen Husnes <sandrahh@stud.ntnu.no>
2021-04-15 20:10:58 +0200

    new song

--- core/src/poi/game/controllers/SoundController.java
+++ core/src/poi/game/controllers/SoundController.java
@@ -11,7 +11,7 @@ public class SoundController {
     long id;
 
     public SoundController() {
-        sound = Gdx.audio.newSound(Gdx.files.internal("soundGame.mp3"));
+        sound = Gdx.audio.newSound(Gdx.files.internal("Main Theme ACNH.mp3"));
         collision = Gdx.audio.newSound(Gdx.files.internal("collision.wav"));
     }
 
