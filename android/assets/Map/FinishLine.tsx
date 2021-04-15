3a1a64ad67fb4375bf588687218f29872b3e2d98
unknown <endrehm@stud.ntnu.no>
2021-04-15 03:10:49 +0200

    AddedFinishline

---
+++ android/assets/Map/FinishLine.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="FinishLine" tilewidth="32" tileheight="33" spacing="2" tilecount="1" columns="1">
+ <image source="../Sprites/finishline.png" width="32" height="33"/>
+</tileset>
