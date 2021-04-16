498c22caf3a0c0b1d7cf5a9e69ab1acb00db060a
Marit <maritfha@stud.ntnu.no>
2021-04-16 14:41:22 +0200

    asset manager

--- core/src/poi/game/controllers/SoundController.java
+++ core/src/poi/game/controllers/SoundController.java
@@ -1,18 +1,25 @@
 package poi.game.controllers;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Sound;
 
 public class SoundController {
 
+    private AssetManager assetManager;
     private Sound sound;
     private Sound collision;
     private Boolean isMuted = false;
     long id;
 
     public SoundController() {
-        sound = Gdx.audio.newSound(Gdx.files.internal("Main Theme ACNH.mp3"));
+        assetManager = new AssetManager();
+        assetManager.load("Main Theme ACNH.mp3", Sound.class);
+        assetManager.finishLoading();
+        //sound = Gdx.audio.newSound(Gdx.files.internal("Main Theme ACNH.mp3"));
+        sound = assetManager.get("Main Theme ACNH.mp3", Sound.class);
         id = sound.loop(1.0f);
+
         collision = Gdx.audio.newSound(Gdx.files.internal("collision.wav"));
     }
 
